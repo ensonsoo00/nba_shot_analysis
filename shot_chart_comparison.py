eaa5520d6b3e50c6d0ddfabac3acbe9e16aefbb9
ensonsoo00 <soo.w@northeastern.edu>
2024-04-07 11:58:15 -0400

    resize shot chart

--- shot_chart_comparison.py
+++ shot_chart_comparison.py
@@ -16,13 +16,11 @@ df_2000_ny = df2000[df2000["team"]=="New York"]
 df_2019_ny = df2019[df2019["team"]=="New York"]
 
 
-fig, ax = plt.subplots(1,2, figsize=(10,10))
-plt.subplots_adjust(left=0.25, bottom=0.25, right=0.75)
+fig, ax = plt.subplots(1,2, figsize=(10,5))
+plt.subplots_adjust(left=0.25, bottom=0.4, right=0.75)
+fig.canvas.set_window_title("Figure: Shot Chart Comparison between 2000 and 2019")
 def shot_charts(df1, df2):
     color1 = df1["outcome"].apply(lambda x: "red" if x=="missed" else "green")
-
-    
-
     ax[0].scatter(df1["x_adj"], df1["y_adj"], marker=".", c=color1, alpha=0.5, s=5)
     ax[0].set_xlim([0,500])
     ax[0].set_ylim([0,500])
@@ -30,13 +28,10 @@ def shot_charts(df1, df2):
     ax[0].imshow(court, cmap="gray")
     ax[0].set_xticks([])
     ax[0].set_yticks([])
-
     ax[0].set_title("NBA 2000 Shot Chart")
 
 
     color2 = df2["outcome"].apply(lambda x: "red" if x=="missed" else "green")
-
-
     ax[1].scatter(df2["x_adj"], df2["y_adj"], marker=".", c=color2, alpha=0.5, s=5)
     ax[1].set_xlim([0,500])
     ax[1].set_ylim([0,500])
@@ -50,7 +45,7 @@ def shot_charts(df1, df2):
 
 axcolor = "white"
 
-rax2000 = plt.axes([0.025, 0.25, 0.15, 0.5], facecolor=axcolor)
+rax2000 = plt.axes([0.025, 0.05, 0.15, 0.9], facecolor=axcolor)
 teams2000 = ["All"] + sorted(list(df2000["team"].unique()))
 radio2000 = RadioButtons(rax2000, teams2000, active=teams2000.index("New York"))
 r = 0.015
@@ -58,7 +53,7 @@ for circ in radio2000.circles:
     circ.width = r * 2.5
     circ.height = r
 
-rax2019 = plt.axes([0.825, 0.25, 0.15, 0.5], facecolor=axcolor)
+rax2019 = plt.axes([0.825, 0.05, 0.15, 0.9], facecolor=axcolor)
 teams2019 = ["All"] + sorted(list(df2019["team"].unique()))
 radio2019 = RadioButtons(rax2019, teams2019, active=teams2019.index("New York"))
 
@@ -71,7 +66,6 @@ def select_team2000(val):
     if val != "All":
         df = df[df["team"]==val]
     color2 = df["outcome"].apply(lambda x: "red" if x=="missed" else "green")
-
     ax[0].cla()
     ax[0].scatter(df["x_adj"], df["y_adj"], marker=".", c=color2, alpha=0.5, s=5)
     ax[0].set_xlim([0,500])
@@ -90,7 +84,6 @@ def select_team2019(val):
     if val != "All":
         df = df[df["team"]==val]
     color2 = df["outcome"].apply(lambda x: "red" if x=="missed" else "green")
-
     ax[1].cla()
     ax[1].scatter(df["x_adj"], df["y_adj"], marker=".", c=color2, alpha=0.5, s=5)
     ax[1].set_xlim([0,500])
